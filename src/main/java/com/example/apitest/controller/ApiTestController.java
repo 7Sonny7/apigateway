14de38f14c8368163e2452d50aece63932ccffe1
jwpark <jwpark@hist.co.kr>
2022-11-10 14:06:23 +0900

    tttt

--- src/main/java/com/example/apitest/controller/ApiTestController.java
+++ src/main/java/com/example/apitest/controller/ApiTestController.java
@@ -4,10 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/sample")
 @RequiredArgsConstructor
@@ -25,5 +22,11 @@ public class ApiTestController {
     @ApiOperation(value = "test api part 2", notes = "test api part 2")
     public String testApi2(){
         return "Second Test API";
+    }
+
+    @GetMapping("/api3")
+    @ApiOperation(value = "test api part 3", notes = "test api part 3")
+    public String testApi3(@RequestParam(required = false) String testParam){
+        return testParam;
     }
 }
